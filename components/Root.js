1904020f25ab6d29c85354ec256e1cc1bd214027
Shari Meggs <smeggs1@gmail.com>
2017-01-03 11:24:55 -0800

    continuting to break up file structure

--- components/Root.js
+++ components/Root.js
@@ -1,45 +1,11 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+//This file will eventually control the state of the application and the navigation
 
+import React, { Component} from 'react';
+import Dashboard from './Dashboard';
 export default class Root extends Component {
-  render() {
+  render(){
     return (
-      <View style={styles.container}>
-      <Text style={styles.welcome}>
-      Hello Shari!
-      </Text>
-      <Text style={styles.instructions}>
-      To get started, edit index.ios.js
-      </Text>
-      <Text style={styles.instructions}>
-      Press Cmd+R to reload,{'\n'}
-      Cmd+D or shake for dev menu
-      </Text>
-      </View>
+      <Dashboard />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
