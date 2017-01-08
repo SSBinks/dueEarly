1204b92b8e7ef89673bc871d23af519614b3447d
Shari Meggs <smeggs1@gmail.com>
2017-01-08 13:50:35 -0800

    Figured out container issues. WIP on a user entering infor for a new assignment

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -47,16 +47,13 @@ class Dashboard extends Component {
     const selection = classes.title + ' ' + classes.time;
     console.log(COURSES[this.state.course].title);
     return (
-
-      <View style={{ marginTop: 20 }}>
-      <Text style={styles.label}> Subject: </Text>
+      <View style={styles.container}>
+      <Text> Subject: </Text>
       <PickerIOS
-      itemStyle={{ fontSize: 25, color: 'red', textAlign: 'left', fontWeight: 'bold' }}
+      // itemStyle={styles.picker}
       selectedValue={this.state.course}
-
       onValueChange={(course) => this.setState({ course })}
       >
-
       {Object.keys(COURSES).map((course) => (
         <PickerItemIOS
         key={course}
@@ -66,46 +63,35 @@ class Dashboard extends Component {
       )
       // console.log(this.state.course);
     )}
-      </PickerIOS>
-
-      </View>
-      // <TextInput
-      // style={{ height: 40, width: 40 }}
-      // placeholder='Here be a list of classes'
-      // OnChangeText={(text) => this.setState({ text })}
-      // />
-
-      // <Text
-      // style={styles.instructions}>
-      // To get started, edit index.ios.js
-      // </Text>
-      // <Text style={styles.instructions}>
-      // Press Cmd+R to reload,{'\n'}
-      // Still Here!
-      // Cmd+D or shake for dev menu
-      // </Text>
-
-
-    );
-  }
+    </PickerIOS>
+    <Text> This course is at {selection} </Text>
+    </View>
+  );
 }
+}
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#838487',
+    flex: 1,
+    marginTop: 65,
+    padding: 5,
   },
   label: {
     fontSize: 20,
-    textAlign: 'center',
+    // textAlign: 'center',
     margin: 10,
     fontFamily: 'Chalkboard SE',
   },
-  instructions: {
+  picker: {
+    fontSize: 25,
+    color: 'black',
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-module.exports = Dashboard;
+    fontWeight: 'bold',
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center'
+
+  }
+  });
+  module.exports = Dashboard;
