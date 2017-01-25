401440afd8c7dcbb20416c6b9443699ac93f0f4b
Shari Meggs <smeggs1@gmail.com>
2017-01-25 14:14:21 -0800

    Can search by category

--- components/Course.js
+++ components/Course.js
@@ -9,11 +9,52 @@ import {
   DatePickerIOS,
   Button,
   ListView,
-
+  PickerIOS,
 
 } from 'react-native';
 
 const moment = require('moment');
+const PickerItemIOS = PickerIOS.Item;
+
+const ASSIGNMENT_TYPES = {
+  default: {
+    type: '',
+    progress: 0
+  },
+  reading: {
+    type: 'Reading',
+    progress: 10
+  },
+  draft: {
+    type: 'Draft',
+    progress: 55
+  },
+  termPaper: {
+    type: 'Term Paper',
+    progress: 30
+  },
+
+  midterm: {
+    type: 'Midterm',
+    progress: 20
+  },
+  final: {
+    type: 'Final',
+    progress: 40
+  },
+  exam: {
+    type: 'Exam',
+    progress: 40
+  },
+  presentation: {
+    type: 'Presentation',
+    progress: 90
+  },
+  personal: {
+    type: 'Personal Project',
+    progress: 20
+  }
+};
 
 class Course extends Component {
   static defaultProps = {
@@ -28,6 +69,8 @@ class Course extends Component {
       date: this.props.date,
       dataSource: this.dailyTask.cloneWithRows(this.assignment),
       dateModal: false,
+      typeModal: false,
+      deliverable: 'default'
     };
     console.log('This is the date: ' + this.state.date)
   }
@@ -36,13 +79,40 @@ class Course extends Component {
     this.setState({ date: date });
   };
   //Modal setting!
-  setdateModal(visible, othercrap) {
+  setdateModal(visible, object) {
     console.log('This is the first thing:' + visible);
-    console.log('This is the second thing:' + othercrap);
+    console.log('This is the second thing:' + object);
     this.setState({ dateModal: visible });
     if (visible === false) {
       this.dateAssignments();
     }
+  }
+
+  setTypeModal(visible, object) {
+    this.setState({ typeModal: visible });
+    if (visible === false) {
+      this.categoryAssignments();
+    }
+  }
+
+  categoryAssignments(){
+    console.log("HIIIII");
+    const category = ASSIGNMENT_TYPES[this.state.deliverable].type;
+    const url = 'http://www.whats-due.com/assign/category/' + category;
+    console.log("The category is " + category);
+    console.log( 'this is the url ' + url);
+    fetch(url, {
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.assignment = responseJson;
+      console.log("Let's go to the Assignment!" + this.assignment);
+      this.setState({ assignments: this.assignment, dataSource: this.dailyTask.cloneWithRows(this.assignment) });
+      return this.assignment;
+    })
+    .catch((error) => {
+      console.error("You don't want zero problems big fella" + error);
+    });
   }
 
   dateAssignments() {
@@ -57,7 +127,7 @@ class Course extends Component {
     .then((response) => response.json())
     .then((responseJson) => {
       this.assignment = responseJson;
-      console.log( "Let's go to the Assignment!" + this.assignment);
+      console.log("Let's go to the Assignment!" + this.assignment);
       this.setState({ assignments: this.assignment, dataSource: this.dailyTask.cloneWithRows(this.assignment) });
       return this.assignment;
     })
@@ -98,30 +168,34 @@ class Course extends Component {
 
 
   render() {
+    const modalBackgroundStyle = {
+      backgroundColor: this.state.typeModal ? 'rgba(0, 0, 0, 0.5)' : 'white',
+    };
     return (
+
       <View style={styles.container}>
-      <View style={{marginBottom: 25}} >
-      <Text style={{ fontWeight: 'bold', fontSize: 20, fontFamily: 'ChalkboardSE-Bold'}} >
+      <View style={{ marginBottom: 25 }} >
+      <Text style={{ fontWeight: 'bold', fontSize: 20, fontFamily: 'ChalkboardSE-Bold' }} >
       I am looking for my task by ...
       </Text>
       </View>
 
       <View style={styles.topMenu}>
       <TouchableHighlight>
-      <View style={{ height: 80, width: 140, alignSelf: 'center'}}>
-      <View style={{ backgroundColor: 'grey', borderRadius: 30, flex: 1, justifyContent: 'center'}}>
-      <Text style={{textAlign: 'center', fontWeight: 'bold'}}> Category </Text>
+      <View style={{ height: 80, width: 140, alignSelf: 'center' }}>
+      <View style={{ backgroundColor: 'grey', borderRadius: 30, flex: 1, justifyContent: 'center' }}>
+      <Text onPress={this.setTypeModal.bind(this, true)} style={{ textAlign: 'center', fontWeight: 'bold' }}> Category </Text>
       </View>
       </View>
       </TouchableHighlight>
 
       <TouchableHighlight>
-      <View style={{ height: 80, width: 140, alignSelf: 'center'}}>
-      <View style={{ backgroundColor: 'grey', borderRadius: 30, flex: 1, justifyContent: 'center'}}
-
+      <View style={{ height: 80, width: 140, alignSelf: 'center' }}>
+      <View
+      style={{ backgroundColor: 'grey', borderRadius: 30, flex: 1, justifyContent: 'center' }}
       >
       <Text
-      style={{textAlign: 'center', fontWeight: 'bold'}}
+      style={{ textAlign: 'center', fontWeight: 'bold' }}
       onPress={this.setdateModal.bind(this, true)}
       > Date </Text>
       </View>
@@ -135,7 +209,7 @@ class Course extends Component {
       transparent={true}
       >
       <View style={[styles.container, { justifyContent: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }]}>
-      <View style={{ backgroundColor: 'white', justifyContent: 'center'}}>
+      <View style={{ backgroundColor: 'white', justifyContent: 'center' }}>
       <Text style={styles.modalHeading}> Choose a Date </Text>
       <View style={{ padding: 5 }}>
       <DatePickerIOS
@@ -165,6 +239,46 @@ class Course extends Component {
       renderRow={this.renderRow.bind(this)}
       />
       </View>
+
+
+      {/* TYPE MODAL*/}
+      <Modal
+      visible={this.state.typeModal}
+      animationType='fade'
+      transparent={true}
+      >
+      <View style={[styles.container, { justifyContent: 'center' }, modalBackgroundStyle]}>
+      <View style={{ backgroundColor: 'white' }}>
+      <View style={{ paddingTop: 5 }}>
+      <Text style={styles.modalHeading}> Assignment Type </Text>
+      </View>
+      <PickerIOS
+      itemStyle={styles.picker}
+      selectedValue={this.state.deliverable}
+      onValueChange={(deliverable) => this.setState({ deliverable })}
+
+      >
+      {Object.keys(ASSIGNMENT_TYPES).map((deliverable) => (
+        <PickerItemIOS
+        key={deliverable}
+        value={deliverable}
+        label={ASSIGNMENT_TYPES[deliverable].type}
+        />
+      )
+      // console.log(this.state.course);
+    )}
+    </PickerIOS>
+    <View style={styles.button}>
+    <Button
+    onPress={this.setTypeModal.bind(this, false)}
+    title='Choose Assignment'
+    color='white'
+    />
+    </View>
+    </View>
+    </View>
+
+    </Modal>
 
       </View>
 
